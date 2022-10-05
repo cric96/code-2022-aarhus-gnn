a8b91b2739e4ac728e4a69b9452a8b011eff12ba
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2022-10-05 17:44:26 +0200

    chore: add tests files

--- tests/test_data_loader.py
+++ tests/test_data_loader.py
@@ -7,31 +7,31 @@ simulations_snapshots = 100
 
 def test_load_simple_data():
     file_size_in_folder = 4
-    loader = PhenomenaDataLoader("./data/subset/")
+    loader = PhenomenaDataLoader("./snapshots/subset/")
     assert len(loader.data) == file_size_in_folder
 
 
 def test_loading_limit():
-    loader = PhenomenaDataLoader("./data/subset/", 1)
+    loader = PhenomenaDataLoader("./snapshots/subset/", 1)
     assert len(loader.data) == 1
 
 
 def test_forecast_window_size():
     window = 5
-    loader = PhenomenaDataLoader("./data/subset/", 1, window)
+    loader = PhenomenaDataLoader("./snapshots/subset/", 1, window)
     snapshot = loader.data[0][0]  # first snapshot
     assert snapshot.y.shape == (nodes, window)
 
 
 def test_clean_position():
-    loader = PhenomenaDataLoader("./data/subset/", 1)
+    loader = PhenomenaDataLoader("./snapshots/subset/", 1)
     loader.clean_position()
     snapshot = loader.data[0][0]
     assert snapshot.x.shape == (nodes, 1)
 
 def test_memory_window():
     window = 5
-    loader = PhenomenaDataLoader("./data/subset/", 1)
+    loader = PhenomenaDataLoader("./snapshots/subset/", 1)
     dataset = loader.fixed_window_size(window)
     snapshot = dataset[0]
     assert snapshot.x.shape == (nodes, window)

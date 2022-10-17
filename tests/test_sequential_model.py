0c8fc4f15c56ff10e440d14d6296e70b43a7bfe3
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2022-10-17 15:58:43 +0200

    fix: improve test

--- tests/test_sequential_model.py
+++ tests/test_sequential_model.py
@@ -5,15 +5,16 @@ from project.data.loader import PhenomenaDataLoader, GraphDatasetIterator
 from project.model.sequential import SpatioTemporalConvolutionGru, SpatioTemporalConvolutionLstm, TemporalGru, SpatialGNN
 from project.model.linear import Linear
 import pytorch_lightning as pl
-loader = PhenomenaDataLoader("./snapshots/subset/", 1)
+forecast_size = 3
+loader = PhenomenaDataLoader("./snapshots/subset/", 1, forecast_size)
 loader.clean_position()
 
 testdata = [
-    SpatioTemporalConvolutionGru(1, 1, 5),
-    SpatioTemporalConvolutionLstm(1, 1, 5),
-    Linear(1, 1, 5),
-    TemporalGru(1, 1, 5),
-    SpatialGNN(1, 1, 5)
+    SpatioTemporalConvolutionGru(1, forecast_size, 5),
+    SpatioTemporalConvolutionLstm(1, forecast_size, 5),
+    Linear(1, forecast_size, 5),
+    TemporalGru(1, forecast_size, 5),
+    SpatialGNN(1, forecast_size, 5)
 ]
 ids = ["spatio temporal gru", "spatio temporal lstm", "feed forward", "temporal gru", "spatial GNN"]
 
@@ -22,7 +23,7 @@ ids = ["spatio temporal gru", "spatio temporal lstm", "feed forward", "temporal 
 def test_model_forward(model):
     snapshot = loader.data[0][0]
     (forward, memory) = model(snapshot.x, snapshot.edge_index, snapshot.edge_attr)
-    assert forward.shape == (400, 1)
+    assert forward.shape == (400, forecast_size)
 
 @pytest.mark.parametrize("model", testdata, ids=ids)
 def test_model_train(model):

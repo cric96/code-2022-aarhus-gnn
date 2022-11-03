e5c76603ad955c2b5a8c2d5ecaa878ccc2518d3d
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2022-11-03 17:10:13 +0100

    feat: update version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gnn-forecast',
-    version='0.0.1',
+    version='0.0.2',
     description='Forecasting of distributed spatio-temporal phenomena using Graph Neural Network + Recurrent Neural Networks',
     author='cric96',
     author_email='gianluca.aguzzi@unibo.it',
